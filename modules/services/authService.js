8d9d88dae388f2fcf4230ab2de6f30f6f5429899
ajha5088 <ajha5088@gmail.com>
2024-05-19 14:15:53 +0530

    Bug : bcryptjs module updated.

--- modules/services/authService.js
+++ modules/services/authService.js
@@ -1,4 +1,4 @@
-const bcrypt = require("bcrypt");
+const bcrypt = require('bcryptjs');
 const jwt = require("jsonwebtoken");
 const uuid = require("uuid");
 const appLogger = require("../../config/appLogger");
